59a8f32a6ec7dd9f92402bfb50fe717a7a7eed84
ffan <ffan@nvidia.com>
2023-09-11 09:46:29 +0800

    add more demos

--- first_go.go
+++ first_go.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
-func main()  {
+func greeting(first_name string, last_name string) (int, int) {
+	fmt.Printf("hello %v %v\n", first_name, last_name)
+	return 1, 3
+}
+
+func main() {
 	name := "ffan"
-	var age int = 40
+	var age int = rand.Intn(100)
 	const total int = 100
 	var books [10]string // define array
 	var s []int
-	books[0] = "cuda"
-	s = append(s, 100)  // demo append a slice
+	books[0] = "ffan"
+	books[1] = "cuda"
+	s = append(s, 100) // demo append a slice
 	var length int = len(books)
+	if age > 30 {
+		// do something
+	} else {
+		//do other things
+		fmt.Println("age < 30\n")
+	}
+	//for _, book := range books {  // placeholder symbol _
+	for index, book := range books { // loop in go
+		fmt.Printf("index: %v, book: %v\n", index, book)
+	}
+	greeting(books[0], books[1])
 	fmt.Println("hello go")
 	fmt.Printf("name = %v, age = %v, length = %v\n", name, age, length)
+
+	return
 }
